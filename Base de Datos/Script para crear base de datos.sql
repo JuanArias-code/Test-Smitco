2098e5d33055f68b6c11d375f8e89dd9f9d8816f
Juan Arias <coorddesarrollo@banasan.com.co>
2024-08-06 00:49:31 -0500

    Data Base

---
+++ Base de Datos/Script para crear base de datos.sql
@@ -0,0 +1,99 @@
+USE [MovieDB]
+GO
+/****** Object:  Table [dbo].[Genre]    Script Date: 06-08-2024 12.43.13 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Genre](
+	[GenreID] [int] IDENTITY(1,1) NOT NULL,
+	[GenreName] [varchar](20) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[GenreID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Movie]    Script Date: 06-08-2024 12.43.13 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Movie](
+	[MovieID] [int] IDENTITY(1,1) NOT NULL,
+	[Title] [varchar](100) NOT NULL,
+	[Overview] [varchar](1024) NOT NULL,
+	[Genre] [varchar](20) NOT NULL,
+	[Language] [varchar](20) NOT NULL,
+	[Duration] [int] NOT NULL,
+	[Rating] [decimal](2, 1) NULL,
+	[PosterPath] [varchar](100) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[MovieID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[UserMaster]    Script Date: 06-08-2024 12.43.13 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserMaster](
+	[UserID] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [varchar](20) NOT NULL,
+	[LastName] [varchar](20) NOT NULL,
+	[Username] [varchar](40) NOT NULL,
+	[Password] [varchar](40) NOT NULL,
+	[Gender] [varchar](6) NOT NULL,
+	[UserTypeName] [varchar](20) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[UserID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[UserType]    Script Date: 06-08-2024 12.43.13 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserType](
+	[UserTypeID] [int] IDENTITY(1,1) NOT NULL,
+	[UserTypeName] [varchar](20) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[UserTypeID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Watchlist]    Script Date: 06-08-2024 12.43.13 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Watchlist](
+	[WatchlistId] [varchar](36) NOT NULL,
+	[UserID] [int] NOT NULL,
+	[DateCreated] [datetime] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[WatchlistId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[WatchlistItems]    Script Date: 06-08-2024 12.43.13 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[WatchlistItems](
+	[WatchlistItemId] [int] IDENTITY(1,1) NOT NULL,
+	[WatchlistId] [varchar](36) NOT NULL,
+	[MovieId] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[WatchlistItemId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
